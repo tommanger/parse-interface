3ed885791715da11c4d7850b1deab723044383f6
tommanger <tom.manger10@gmail.com>
2020-12-28 15:14:06 +0200

    first commit

---
+++ lib/parseInterface.ts
@@ -0,0 +1,82 @@
+export function parseInterface(interfaceToParse: string): {name: string, obj: {[index: string]: any}, unknown: {key: string, type: string}[]}[] {
+    const splited = interfaceToParse.split("interface");
+    let jsons = [];
+    splited.forEach((row) => {
+        const splitedRow = row.split("\n");
+        const name = splitedRow[0].split("{")[0].trim();
+        const obj = {}, unknown = [];
+
+        let fieldIsObj = false, objKey = "";
+        for (let field of splitedRow) {
+            let [key, type] = field.split(":");
+            if ((key && key.includes("}")) || (type && type.includes("}"))) {
+                fieldIsObj = false;
+                objKey = "";
+                continue;
+            }
+
+            if (!key || !type) {
+                continue;
+            }
+
+            key = key.trim();
+            key = key.split("?")[0];
+            type = type.trim();
+            let value = null;
+            switch (type) {
+                case "number":
+                case "number;":
+                case "number | string":
+                case "number | string;":
+                    value = 0;
+                    break;
+                case "string":
+                case "string;":
+                case "string | number":
+                case "string | number;":
+                    value = "";
+                    break;
+                case "boolean":
+                case "boolean;":
+                    value = false;
+                    break;
+                case "{":
+                    value = {};
+                    objKey = key;
+                    fieldIsObj = true;
+                    break;
+                case "Partial<CSSStyleDeclaration>":
+                case "Partial<CSSStyleDeclaration>;":
+                case "CSSStyleDeclaration":
+                    value = {};
+                    break;
+            }
+
+            if (value !== null) {
+                if (fieldIsObj && type !== "{") {
+                    obj[objKey][key] = value;
+                } else {
+                    obj[key] = value;
+                }
+            } else {
+                const splitedType = type.split("|");
+                if (splitedType.length > 1) {
+                    try {
+                        obj[key] = JSON.parse(splitedType[0].trim());
+                    } catch (e) {
+                        obj[key] = splitedType[0].trim();
+                    }
+                }
+
+                if (!obj[key]) {
+                    unknown.push({key, type})
+                }
+            }
+        }
+        if (Object.keys(obj).length !== 0 && obj.constructor === Object && name) {
+            jsons.push({obj, name, unknown});
+        }
+    })
+
+    return jsons;
+}
